781103cc1d9825072b97b57b57801278b9990706
Ziad AK <ziadakgit@gmail.com>
2024-01-17 23:03:06 +0200

    cleanup

--- submit_quiz.js
+++ submit_quiz.js
@@ -1,46 +1,50 @@
-let submit_checker = localStorage.getItem('submited');
-let dogs_checker = localStorage.getItem('dogs_chosen');
+let submit_checker = localStorage.getItem("submited");
+let dogs_checker = localStorage.getItem("dogs_chosen");
 let dogs_result = [];
 function submitQuiz() {
-    const user_choices = {};
-    const inputs = document.getElementsByTagName("input");
-    for (let i = 0; i < inputs.length; i++) {
-        if (inputs[i].checked) {
-            user_choices[inputs[i].name] = parseInt(inputs[i].value);
-        }
-    }
-    fetch(`${currentProtocol}//${currentHost}:5000/api/submit_quiz`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(user_choices),
-    })
-        .then((response) => response.json())
-        .then((dogs) => {
-            for (let dog in dogs) {
-                const chosen_dog = [
-                    dog, {
-                        description: dogs[dog]["description"],
-                        temperament: dogs[dog]["temperament"],
-                        breed: dogs[dog]["breed"],
-                        age: dogs[dog]["age"],
-                        attributes: dogs[dog]["attributes"]
-                    },
-                ];
-                dogs_result.push(chosen_dog);
-            }
-            localStorage.setItem('submited', "Quiz Submited");
-            localStorage["dogs_chosen"] = JSON.stringify(dogs_result);
-            alert("Quiz Submited :)\nGoing to the Dashboard page to see your result 🐾");
-            window.location.href.replace("dashboard.html");
-        })
-        .catch((error) => {
-            localStorage.setItem('submited', false);
-            console.error("Error in fetch:", error);
-        });
+	const user_choices = {};
+	const inputs = document.getElementsByTagName("input");
+	for (let i = 0; i < inputs.length; i++) {
+		if (inputs[i].checked) {
+			user_choices[inputs[i].name] = parseInt(inputs[i].value);
+		}
+	}
+	fetch(`${currentProtocol}//${currentHost}:5000/api/submit_quiz`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(user_choices),
+	})
+		.then((response) => response.json())
+		.then((dogs) => {
+			for (let dog in dogs) {
+				const chosen_dog = [
+					dog,
+					{
+						description: dogs[dog]["description"],
+						temperament: dogs[dog]["temperament"],
+						breed: dogs[dog]["breed"],
+						age: dogs[dog]["age"],
+						attributes: dogs[dog]["attributes"],
+					},
+				];
+				dogs_result.push(chosen_dog);
+			}
+			localStorage.setItem("submited", "Quiz Submited");
+			localStorage["dogs_chosen"] = JSON.stringify(dogs_result);
+			alert(
+				"Quiz Submited :)\nGoing to the dashboard page to see your result 🐾"
+			);
+			window.location.href = "dashboard.html";
+		})
+		.catch((error) => {
+			localStorage.setItem("submited", false);
+			console.error("Error in fetch:", error);
+		});
 }
 function resetQuiz() {
-    localStorage["submited"] = "Not submited";
-    location.reload();
+	localStorage["submited"] = "Not submited";
+	alert("Localstorage reseted\nA new quiz has been started!");
+	location.reload();
 }
